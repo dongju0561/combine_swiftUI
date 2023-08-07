4e2c0bc1de6288c4c5de1685813cffa002b0af44
DongjuPark <pdj95044@gmail.com>
2023-08-07 22:43:14 +0900

    Initial Commit

---
+++ combine_swiftUI_practice/combine_swiftUI_practiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  combine_swiftUI_practiceApp.swift
+//  combine_swiftUI_practice
+//
+//  Created by Dongju Park on 2023/08/07.
+//
+
+import SwiftUI
+
+@main
+struct combine_swiftUI_practiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
